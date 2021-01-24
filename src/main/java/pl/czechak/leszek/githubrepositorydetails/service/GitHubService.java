07efd9eff6e78a18276ad6f9c5e86b39eacfe217
Leszek Czechak <leszekczechak@gmail.com>
2021-01-24 13:18:13 +0100

    GitHubServiceTest -work in progress

--- src/main/java/pl/czechak/leszek/githubrepositorydetails/service/GitHubService.java
+++ src/main/java/pl/czechak/leszek/githubrepositorydetails/service/GitHubService.java
@@ -10,8 +10,9 @@ import reactor.core.publisher.Mono;
 @Service
 public class GitHubService {
 
-    @Value("${github.user.token}")
-    private String userToken;
+//    @Value("${github.user.token}")
+//    wartość wpisana  ręcznie na czas testów
+    private String userToken = "965f496c7ac6bbf37c67f4e9d0f3f5194528d837";
 
     private final WebClient webClient;
 
@@ -23,8 +24,8 @@ public class GitHubService {
 
         return webClient.get()
                 .uri("/" + owner + "/" + repositoryName)
-                .header(HttpHeaders.ACCEPT,"application/vnd.github.v3+json")
-                .header(HttpHeaders.AUTHORIZATION, "token "+userToken)
+                .header(HttpHeaders.ACCEPT, "application/vnd.github.v3+json")
+                .header(HttpHeaders.AUTHORIZATION, "token " + userToken)
                 .retrieve()
                 .bodyToMono(Details.class);
     }
--- src/test/java/pl/czechak/leszek/githubrepositorydetails/service/GitHubServiceTest.java
+++ src/test/java/pl/czechak/leszek/githubrepositorydetails/service/GitHubServiceTest.java
@@ -8,8 +8,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 import pl.czechak.leszek.githubrepositorydetails.model.Details;
 import reactor.core.publisher.Mono;
@@ -18,17 +18,14 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 
 @ExtendWith(MockitoExtension.class)
 class GitHubServiceTest {
 
-    @Mock
-    private WebClient webClient;
-
     public static MockWebServer mockWebServer;
-
-    private ObjectMapper objectMapper;
     private GitHubService gitHubService;
+    private ObjectMapper objectMapper;
 
     @BeforeAll
     static void setUp() throws IOException {
@@ -42,13 +39,19 @@ class GitHubServiceTest {
     }
 
     @BeforeEach
-    void setUpService() {
-        String baseUrl = String.format("http://localhost:%s", //Nie wiem jak przypisać ten URL do gitHubService
+    void initialize() {
+        String baseUrl = String.format("http://localhost:%s",
                 mockWebServer.getPort());
-        objectMapper = new ObjectMapper();
-        gitHubService = new GitHubService(webClient);
-    }
 
+        WebClient webClient = WebClient.builder()
+                .baseUrl(baseUrl)
+                .defaultHeader("Content-Type", "application/json")
+                .build();
+
+        gitHubService = new GitHubService(webClient);
+
+        objectMapper = new ObjectMapper();
+    }
 
     @Test
     void shouldReturnDetailsByOwnerAndRepositoryName() throws IOException {
@@ -56,23 +59,28 @@ class GitHubServiceTest {
         //given
         String repositoryName = "repoName";
         String owner = "username";
-        String userToken = "";
         Details details = new Details(repositoryName,
                 "descTest",
                 "urlTest",
                 16,
-                LocalDateTime.MAX);
+                LocalDateTime.now());
 
         Mono<Details> detailsMono = Mono.just(details);
 
-        mockWebServer.enqueue(new MockResponse()
-                .setBody(objectMapper.writeValueAsString(details))
-                .addHeader("Content-Type", "application/json"));
+        String body = objectMapper.writeValueAsString(details);
+
+        MockResponse mockResponse = new MockResponse()
+                .clearHeaders()
+                .addHeader(CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .setBody(body);
+
+        mockWebServer.enqueue(mockResponse);
 
         //when
-        Mono<Details> response = gitHubService.getDetails(owner, repositoryName);
+        Mono<Details> responseMono = gitHubService.getDetails(owner, repositoryName);
 
         //then
-        assertThat(response).isEqualTo(detailsMono);
+        Details response = responseMono.block();
+        assertThat(responseMono).isEqualTo(detailsMono);
     }
 }
